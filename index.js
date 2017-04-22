4bb754e82d08444b1e2b080087fe399bfe330ee1
Luca Vidale <luca@crispybacon.it>
2017-04-22 08:10:49 +0200

    Your Message

--- index.js
+++ index.js
@@ -5,13 +5,11 @@ var app = express();
 var googleAuth = require('google-auth-library');
 var auth = new googleAuth();
 var urlDecoder = require('base64-url');
-
 var YOUR_CLIENT_ID = "114086584296-3um4j3ne41m27qtcqg4bupc10lqnk1mn.apps.googleusercontent.com";
 var YOUR_CLIENT_SECRET = "NFXIFTuqMS74Crid-CiiD9N_";
 var YOUR_REDIRECT_URL = "http://cattolica.crispybacon.us:5000/oauth2callback";
 var oauth2Client = new auth.OAuth2(YOUR_CLIENT_ID, YOUR_CLIENT_SECRET, YOUR_REDIRECT_URL);
-
-// generate a url that asks permissions for Google+ and Google Calendar scopes
+// generate a url that asks permissions for GoogleMail and Google Calendar scopes
 var scopes = [
   'https://www.googleapis.com/auth/gmail.readonly',
   'https://www.googleapis.com/auth/gmail.send'
@@ -30,18 +28,16 @@ app.get('/', function (req, res) {
 });
 
 app.get('/oauth2callback', function (req, res) {
-  console.log("CODE=> "+ req.query.code);
   oauth2Client.getToken(req.query.code, function (err, tokens) {
-    console.log("TOKENS=> "+tokens);
     if (!err) {
-      console.log('tokens=>  '+ JSON.stringify(tokens));
       oauth2Client.setCredentials(tokens);
       res.end(JSON.stringify(tokens));
     }
   });
 });
 
-app.get('/auth/10', function(req,res){
+// web browser authentication
+app.get('/auth/web', function(req,res){
   var gmail = google.gmail('v1');
   gmail.users.messages.list({
     auth: oauth2Client,
@@ -49,10 +45,8 @@ app.get('/auth/10', function(req,res){
     maxResults: 1
   }, function(err, response) {
     if (err) {
-      console.log('The API returned an error: ' + err);
       res.json({result:"error", data:err});
     }
-    console.log("RESPONSE => " + JSON.stringify(response));
     var messages = response.messages;
     if (messages.length == 0) {
       console.log('No messages found.');
@@ -68,11 +62,9 @@ app.get('/auth/10', function(req,res){
           id: message.id,
           format: "raw"
         }, function(err, messageResponse) {
-          console.log("SNIPPET=> "+JSON.stringify(messageResponse.snippet));
-          console.log("LabelsIDs=> "+JSON.stringify(messageResponse.labelIds));
           //console.log("MESSAGE=> "+JSON.stringify(messageResponse));
-          var result = urlDecoder.decode(messageResponse.raw);
-          console.log(result);
+          var eMailBodyParsed = urlDecoder.decode(messageResponse.raw);
+          console.log(eMailBodyParsed);
           res.json({result:"success"});
         });
       }
@@ -80,12 +72,12 @@ app.get('/auth/10', function(req,res){
   });
 });
 
+
+//mobile app authentication
 app.get('/auth', function (req, res) {
   var code = req.query.code;
   oauth2Client.getToken(req.query.code, function (err, tokens) {
-    console.log("TOKENS=> "+tokens);
     if (!err) {
-      console.log('tokens=>  '+ JSON.stringify(tokens));
       oauth2Client.setCredentials(tokens);
       var gmail = google.gmail('v1');
       gmail.users.messages.list({
@@ -97,15 +89,12 @@ app.get('/auth', function (req, res) {
           console.log('The API returned an error: ' + err);
           res.json({result:"error", data:err});
         }
-        console.log("RESPONSE => " + JSON.stringify(response));
         var messages = response.messages;
         if (messages.length == 0) {
           console.log('No messages found.');
         } else {
           console.log('Messages:');
           for (var i = 0; i < messages.length; i++) {
-            var message = messages[i];
-            console.log('- %s', message.id);
             var message = messages[i];
             gmail.users.messages.get({
               auth: oauth2Client,
@@ -115,9 +104,8 @@ app.get('/auth', function (req, res) {
             }, function(err, messageResponse) {
               console.log("SNIPPET=> "+JSON.stringify(messageResponse.snippet));
               console.log("LabelsIDs=> "+JSON.stringify(messageResponse.labelIds));
-              //console.log("MESSAGE=> "+JSON.stringify(messageResponse));
-              var result = urlDecoder.decode(messageResponse.raw);
-              console.log(result);
+              var eMailBodyParsed = urlDecoder.decode(messageResponse.raw);
+              console.log(eMailBodyParsed);
               res.json({result:"success"});
             });
           }
@@ -134,12 +122,12 @@ var onesignal_client = onesignal.createClient();
 var restApiKey = 'Y2U0Mjg5YmEtY2U5NC00NzQyLTgzNWUtOWNjOTY0OWFhOGE4';
 
 app.get("/send",function(req,res){
-  sendNotification("",function(response){
+  sendNotification("No Sbatty - contro il furto","Dalle tue email ho visto che dopodomani sarai a Parigi, perchè non ti assicuri contro il furto del tuo nuovo PC? Sai di questi tempi non si sa mai!",function(response){
     res.end(response);
   });
 });
 
-function sendNotification(message,callback){
+function sendNotification(title,message,callback){
   var params = {
       app_id: 'f95c5a82-2a74-40d2-9f77-47177bf782f8',
       included_segments:["All"],
@@ -157,10 +145,10 @@ function sendNotification(message,callback){
   onesignal_client.notifications.create(restApiKey, params, function (err, response) {
       if (err) {
       	console.log('Encountered error', err);
-        callback(response);
+        callback("error");
     	} else {
       	console.log(response);
-        callback("");
+        callback("success");
     	}
   });
 }
